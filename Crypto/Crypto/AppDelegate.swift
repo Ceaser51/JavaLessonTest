91b5a332728fb59248b8ca1364866bb70f934ce8
Ceaser51 <116198970+Ceaser51@users.noreply.github.com>
2020-07-18 13:37:11 +0000

    Fixed minor bug

--- Crypto/Crypto/AppDelegate.swift
+++ Crypto/Crypto/AppDelegate.swift
@@ -5,4 +5,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOp
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override 
