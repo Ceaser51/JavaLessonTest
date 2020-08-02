fad518622d9b3afc47e5d3dad3621b3f04a5f1dd
Ceaser51 <116198970+Ceaser51@users.noreply.github.com>
2020-08-02 23:08:20 +0000

    Fix ESLint warnings

--- Crypto/Crypto/AppDelegate.swift
+++ Crypto/Crypto/AppDelegate.swift
@@ -18,4 +18,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISce
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user disc
