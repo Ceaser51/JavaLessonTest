25e721f13fd385c47f8e3f16efefc54b8a46f210
Ceaser51 <116198970+Ceaser51@users.noreply.github.com>
2020-08-05 08:14:11 +0000

    update proto

--- Crypto/Crypto/AppDelegate.swift
+++ Crypto/Crypto/AppDelegate.swift
@@ -19,4 +19,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user disc
+        // Called when the user discards a scene session.
+        // If any sessions we
