35ada72c58434553dd60c3fe377a7d148d60f2df
Ceaser51 <116198970+Ceaser51@users.noreply.github.com>
2020-12-10 11:20:09 +0000

    Removed tabs.

--- Crypto/Crypto/ContentView.swift
+++ Crypto/Crypto/ContentView.swift
@@ -6,4 +6,6 @@ struct GraphCoin: View {
     let lineCoordinates: [CGFloat]
     
     var body: some  View {
-        LineChartController(lineCoordinates: lineCoordina
+        LineChartController(lineCoordinates: lineCoordinates, inline: false)
+            .padding(.leading, 30)
+        .na
