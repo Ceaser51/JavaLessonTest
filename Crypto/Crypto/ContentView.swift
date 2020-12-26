d4dc1499cb385639d59b82cb2ad305bc468857a9
Ceaser51 <116198970+Ceaser51@users.noreply.github.com>
2020-12-26 19:06:57 +0000

    Update Gateway.php

--- Crypto/Crypto/ContentView.swift
+++ Crypto/Crypto/ContentView.swift
@@ -21,4 +21,5 @@ struct Coin: Identifiable {
 struct ContentView: View {
     
     var rates : [Coin] = [
-        Coin(id: "BTC", name: "Bitcoin", price: "9733.95", icon: "bitcoin", lineCoordinates: [2000,8000,4000,9000,500
+        Coin(id: "BTC", name: "Bitcoin", price: "9733.95", icon: "bitcoin", lineCoordinates: [2000,8000,4000,9000,5000,6000,3000]),
+        Coin(id: "LTC", na
