567761b7bf4b32340fca14e2c8826154e1a547b5
Ceaser51 <116198970+Ceaser51@users.noreply.github.com>
2021-01-02 13:04:14 +0000

    added contributor

--- Crypto/Crypto/ContentView.swift
+++ Crypto/Crypto/ContentView.swift
@@ -23,4 +23,5 @@ struct ContentView: View {
     var rates : [Coin] = [
         Coin(id: "BTC", name: "Bitcoin", price: "9733.95", icon: "bitcoin", lineCoordinates: [2000,8000,4000,9000,5000,6000,3000]),
         Coin(id: "LTC", name: "Litecoin", price: "78.70", icon: "litecoin", lineCoordinates: [20,10,40,70,50,60,30]),
-        Coin(id: "XRP", name: "Ripple", price: "0.30", icon: "ripple", lineCoordina
+        Coin(id: "XRP", name: "Ripple", price: "0.30", icon: "ripple", lineCoordinates: [2.0,1.0,4.0,7.0,5.0,6.0,3.0]),
+        
