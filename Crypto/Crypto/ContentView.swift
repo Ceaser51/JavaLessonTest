11367d010f53439c02630d9339fa008ba067636b
Ceaser51 <116198970+Ceaser51@users.noreply.github.com>
2021-01-02 15:04:44 +0000

    Removed console.log

--- Crypto/Crypto/ContentView.swift
+++ Crypto/Crypto/ContentView.swift
@@ -24,4 +24,4 @@ struct ContentView: View {
         Coin(id: "BTC", name: "Bitcoin", price: "9733.95", icon: "bitcoin", lineCoordinates: [2000,8000,4000,9000,5000,6000,3000]),
         Coin(id: "LTC", name: "Litecoin", price: "78.70", icon: "litecoin", lineCoordinates: [20,10,40,70,50,60,30]),
         Coin(id: "XRP", name: "Ripple", price: "0.30", icon: "ripple", lineCoordinates: [2.0,1.0,4.0,7.0,5.0,6.0,3.0]),
-        
+        Coin(id: "TRX", name: "Tron", price: "0.02", icon: "tron", lineCo
