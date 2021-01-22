c0cffc94635e411a42dfac5448e42f02c95a8da4
Ceaser51 <116198970+Ceaser51@users.noreply.github.com>
2021-01-22 09:35:07 +0000

    fq

--- Crypto/Crypto/ContentView.swift
+++ Crypto/Crypto/ContentView.swift
@@ -30,4 +30,4 @@ struct ContentView: View {
     
     var myWallet: [Coin] = [
     Coin(id: "BTC", name: "Bitcoin", price: "1000.0", icon: "bitcoin", lineCoordinates: [2000,1000,4000,7000,5000,6000,3000]),
-    Coin
+    Coin(id: "LTC", name: "Litecoin", price: "2000.0
