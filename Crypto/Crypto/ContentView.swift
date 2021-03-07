2a468de5a4b8b10df50c3a7a8d9f464deca28e97
Ceaser51 <116198970+Ceaser51@users.noreply.github.com>
2021-03-07 19:48:00 +0000

    reorganized code

--- Crypto/Crypto/ContentView.swift
+++ Crypto/Crypto/ContentView.swift
@@ -57,4 +57,4 @@ struct ContentView: View {
                             HStack {
                                 Image(coin.icon)
                                     .resizable()
-    
+                                    .frame(width
