544a62f5e6d6b59754d263e928bf6d8409b30406
Ceaser51 <116198970+Ceaser51@users.noreply.github.com>
2021-04-11 10:13:47 +0000

    Add unit testing

--- Crypto/Crypto/ContentView.swift
+++ Crypto/Crypto/ContentView.swift
@@ -94,4 +94,7 @@ struct ContentView: View {
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some Vi
+    static var previews: some View {
+        ContentView()
+    }
+}
