f47341c67c9271ef47cea2daac0bb8e83190ab13
Ceaser51 <116198970+Ceaser51@users.noreply.github.com>
2021-05-29 08:45:11 +0000

    Routes

--- Crypto/Crypto/LineChart.swift
+++ Crypto/Crypto/LineChart.swift
@@ -16,3 +16,52 @@ open class LineChart: UIView {
      * Helpers class
      */
     fileprivate class Helpers {
+        
+        /**
+         * Convert hex color to UIColor
+         */
+        fileprivate class func UIColorFromHex(_ hex: Int) -> UIColor {
+            let red = CGFloat((hex & 0xFF0000) >> 16) / 255.0
+            let green = CGFloat((hex & 0xFF00) >> 8) / 255.0
+            let blue = CGFloat((hex & 0xFF)) / 255.0
+            return UIColor(red: red, green: green, blue: blue, alpha: 1)
+        }
+        
+        /**
+         * Lighten color.
+         */
+        fileprivate class func lightenUIColor(_ color: UIColor) -> UIColor {
+            var h: CGFloat = 0
+            var s: CGFloat = 0
+            var b: CGFloat = 0
+            var a: CGFloat = 0
+            color.getHue(&h, saturation: &s, brightness: &b, alpha: &a)
+            return UIColor(hue: h, saturation: s, brightness: b * 1.5, alpha: a)
+        }
+    }
+    
+    public struct Labels {
+        public var visible: Bool = true
+        public var values: [String] = []
+    }
+    
+    public struct Grid {
+        public var visible: Bool = true
+        public var count: CGFloat = 10
+        // #eeeeee
+        public var color: UIColor = UIColor(red: 238/255.0, green: 238/255.0, blue: 238/255.0, alpha: 1)
+    }
+    
+    public struct Axis {
+        public var visible: Bool = true
+        // #607d8b
+        public var color: UIColor = UIColor(red: 96/255.0, green: 125/255.0, blue: 139/255.0, alpha: 1)
+        public var inset: CGFloat = 15
+    }
+    
+    public struct Coordinate {
+        // public
+        public var labels: Labels = Labels()
+        public var grid: Grid = Grid()
+        public var axis: Axis = Axis()
+        
