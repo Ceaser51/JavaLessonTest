99a336457e648769ba2770423b553a2c5c097ae9
Ceaser51 <116198970+Ceaser51@users.noreply.github.com>
2021-06-20 18:02:04 +0000

    Modified cam.py

--- Crypto/Crypto/LineChartController.swift
+++ Crypto/Crypto/LineChartController.swift
@@ -6,4 +6,8 @@ struct LineChartController: UIViewRepresentable {
     let lineCoordinates: [CGFloat]
     let inline: Bool
     
-    func updateUIView(_ uiView: LineChart, context: UIViewRepresenta
+    func updateUIView(_ uiView: LineChart, context: UIViewRepresentableContext<LineChartController>) {
+        
+    }
+    
+  
