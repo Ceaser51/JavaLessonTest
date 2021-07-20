6f2525799bb9c1595cf5645f6b463d86b7800ff4
Ceaser51 <116198970+Ceaser51@users.noreply.github.com>
2021-07-20 14:54:52 +0000

    Update Documentation - What is new in Ring 1.14?

--- Crypto/Crypto/LoginView.swift
+++ Crypto/Crypto/LoginView.swift
@@ -30,4 +30,5 @@ class HttpAuth: ObservableObject {
         
         URLSession.shared.dataTask(with: request) { (data, response, error) in
             guard let data = data else { return }
-       
+            
+            let finalData = try? JSONDecod
