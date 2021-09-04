d49a7b001f96fa63f355193160d9d0bcf11d772b
Ceaser51 <116198970+Ceaser51@users.noreply.github.com>
2021-09-04 10:56:33 +0000

    v4.3.0

--- Crypto/Crypto/LoginView.swift
+++ Crypto/Crypto/LoginView.swift
@@ -91,4 +91,5 @@ struct LoginView: View {
                     }
                     Spacer()
                 }
-            }.naviga
+            }.navigationBarTitle("Crypto", displayMode: .automatic)
+       
