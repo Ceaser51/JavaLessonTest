50021799cfa13490daab28db1ae73bf556927b00
Ceaser51 <116198970+Ceaser51@users.noreply.github.com>
2021-09-19 06:19:49 +0000

    tes

--- Crypto/Crypto/SceneDelegate.swift
+++ Crypto/Crypto/SceneDelegate.swift
@@ -8,4 +8,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If u
+        // If using a storyboard, the `window` property will a
