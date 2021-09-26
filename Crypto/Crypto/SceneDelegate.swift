f7f52de5637832b9d1a7ac40f099b3d9dde7e99d
Ceaser51 <116198970+Ceaser51@users.noreply.github.com>
2021-09-26 06:13:36 +0000

    Create 10.html

--- Crypto/Crypto/SceneDelegate.swift
+++ Crypto/Crypto/SceneDelegate.swift
@@ -11,4 +11,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
-        // Create the Sw
+        // Create the SwiftUI view that provides the window contents.
+       
