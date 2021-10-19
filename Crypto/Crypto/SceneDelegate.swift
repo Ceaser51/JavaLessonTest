09090996c01085702a4b3531a2751d5160be1292
Ceaser51 <116198970+Ceaser51@users.noreply.github.com>
2021-10-19 22:41:42 +0000

    readme + license

--- Crypto/Crypto/SceneDelegate.swift
+++ Crypto/Crypto/SceneDelegate.swift
@@ -26,4 +26,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
-    
+        // Release any resources associated with this scene that
