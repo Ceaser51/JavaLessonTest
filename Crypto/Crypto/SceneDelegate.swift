87bd332f5f031facf137f1b6a10fee72a4788fa5
Ceaser51 <116198970+Ceaser51@users.noreply.github.com>
2021-10-24 19:05:37 +0000

    Add report-card

--- Crypto/Crypto/SceneDelegate.swift
+++ Crypto/Crypto/SceneDelegate.swift
@@ -31,4 +31,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active s
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that 
