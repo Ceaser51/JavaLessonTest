0c662560bd614acfefe3a7f0838f6268c1c9bbf7
Ceaser51 <116198970+Ceaser51@users.noreply.github.com>
2021-11-02 18:22:45 +0000

    Formatting Changes

--- Crypto/Crypto/SceneDelegate.swift
+++ Crypto/Crypto/SceneDelegate.swift
@@ -45,4 +45,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to undo the changes made on entering the background.
     }
 
-    func sceneDidEnterBackground(_
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the 
