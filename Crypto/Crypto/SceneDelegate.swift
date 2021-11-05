d7ccef438fa55bd523595b4bdef3992d3094a60e
Ceaser51 <116198970+Ceaser51@users.noreply.github.com>
2021-11-05 08:41:45 +0000

    nfs: fix pnfs direct write memory leak

--- Crypto/Crypto/SceneDelegate.swift
+++ Crypto/Crypto/SceneDelegate.swift
@@ -47,4 +47,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough sce
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back 
